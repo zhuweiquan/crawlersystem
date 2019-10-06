17377f940b6452e1170f83a6b09024406cfec187
weiquan <865807189@qq.com>
2019-10-06 20:36:46 +0800

    初始化项目

---
+++ src/main/java/aproject/acrawler/AcrawlerApplication.java
@@ -0,0 +1,13 @@
+package aproject.acrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aproject/acrawler/AcrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package aproject.acrawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcrawlerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
