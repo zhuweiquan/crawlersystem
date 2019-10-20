b9345ec4c720e01c07508b90162a9523f24c59cd
weiquan <865807189@qq.com>
2019-10-21 00:16:14 +0800

    [description] 创建httpclient请求通用方法

--- src/main/java/aproject/acrawler/common/Downloader.java
+++ src/main/java/aproject/acrawler/common/Downloader.java
@@ -1,6 +1,7 @@
 package aproject.acrawler.common;
 
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.apache.http.Header;
 import org.apache.http.HeaderElement;
 import org.apache.http.HttpEntity;
@@ -13,6 +14,7 @@ import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.NameValuePair;
 import org.apache.http.ProtocolException;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -23,6 +25,7 @@ import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
@@ -32,7 +35,9 @@ import org.apache.http.util.EntityUtils;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Downloader {
@@ -43,6 +48,10 @@ public class Downloader {
     private static final String DEFAULT_UA_PC_CHROME = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36";
     private final Map<String, String> cacheCookieMap = new HashMap();
 
+    public Downloader() {
+
+    }
+
     public Downloader(DownloadSetting downloadSetting) {
         this.downloadSetting = downloadSetting;
     }
@@ -50,21 +59,61 @@ public class Downloader {
 
 
     public String processUrl(String url) {
-        String response = null;
-
+        CloseableHttpClient client = null;
+        String res = null;
+        HttpGet httpGet = new HttpGet(url);
         try {
-            CloseableHttpClient client = HttpClientBuilder.create().build();
+            client = HttpClientBuilder.create().build();
+            httpGet.setHeader("User-Agent", DEFAULT_UA_PC_CHROME);
+            httpGet.setHeader(new BasicHeader("Content-Type", "application/json; charset=utf-8"));
+            httpGet.setHeader(new BasicHeader("Accept", "text/plain;charset=utf-8"));
+
+            HttpResponse response = client.execute(httpGet);
+            // 获取响应码
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode == 200) {
+                res = EntityUtils.toString(response.getEntity(), "utf-8");
+            }
+            res = EntityUtils.toString(response.getEntity());
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return response;
+        return res;
     }
 
     public String processUrl(String url,  Map<String, String> postData) {
+        CloseableHttpClient client = null;
         String res = null;
+        try {
+            client = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(url);
+            post.setHeader("User-Agent", DEFAULT_UA_PC_CHROME);
+            post.setHeader("Content-Type", "application/json;charset=UTF-8");
+
+            List<NameValuePair> params = new ArrayList<>();
+            if (!postData.isEmpty()) {
+                for (String key : postData.keySet()) {
+                    params.add(new BasicNameValuePair(key, postData.get(key)));
+                }
+            }
+            post.setEntity(new UrlEncodedFormEntity(params, "utf-8"));
+            HttpResponse response = client.execute(post);
+            res = EntityUtils.toString(response.getEntity());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return res;
     }
+
+//    public static void main(String[] args) {
+//        Downloader downloader = new Downloader();
+//        Map<String, String> map = new HashMap<>();
+//        String url = "http://blog.sina.com.cn/s/blog_6e47de3e01017ole.html";
+//        String s = downloader.processUrl(url);
+//        System.out.println(s);
+//    }
 
 }
