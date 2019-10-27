d5863b9e10237f813f2c6e95ffbb7dcb124ff6af
weiquan <865807189@qq.com>
2019-10-27 21:50:01 +0800

    [description] 定义通用结果返回类及返回码

---
+++ src/main/java/aproject/acrawler/common/Result.java
@@ -0,0 +1,56 @@
+package aproject.acrawler.common;
+
+/**
+ * 接口结果返回通用类
+ * create time : 2019/10/27
+ * author : zhuweiquan
+ * @param <T>
+ */
+public class Result<T> {
+    private Integer code;
+    private String msg;
+    private T data;
+
+    public Result() {
+        super();
+    }
+
+    public Result(Integer code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
---
+++ src/main/java/aproject/acrawler/common/ResultEnum.java
@@ -0,0 +1,31 @@
+package aproject.acrawler.common;
+
+/**
+ * 结果返回码
+ * create time : 2019/10/27
+ * author : zhuweiquan
+ */
+public enum ResultEnum {
+
+    FAIL(-1,"未知错误"),
+    SUCCESS(0,"成功"),
+    USER_NOT_EXIST(1,"用户不存在"),
+    USER_IS_EXISTS(2,"用户已存在"),
+    DATA_IS_NULL(3,"数据为空");
+
+    private Integer code;
+    private String msg;
+
+    ResultEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
